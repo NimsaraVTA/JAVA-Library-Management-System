3f2820a0acfe7f1164babf56298ceea08cc38c7e
Thathsarani Nimsara Narasinghe Bandara <89225439+NimsaraVTA@users.noreply.github.com>
2021-09-30 22:56:24 +0530

    Add files via upload

---
+++ src/library_management_system_orgi/LIBRARY_MANAGEMENT_SYSTEM_ORGI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library_management_system_orgi;
+
+/**
+ *
+ * @author Thathsarani Nimsara
+ */
+public class LIBRARY_MANAGEMENT_SYSTEM_ORGI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
